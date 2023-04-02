17ae1b4881901b0d9f1c43337e857336a7a4ef18
n-miyamoto <scotsdowneroad@gmail.com>
2023-04-02 12:10:46 +0900

    add example

---
+++ examples/secrets.rs
@@ -0,0 +1,5 @@
+pub mod ambient {
+    pub const CHANNEL_ID:u32 = 12345;
+    pub const WRITE_KEY:&str = "0123456789abcdef";
+}
+
---
+++ examples/send_data.rs
@@ -0,0 +1,42 @@
+use ambient_rust::{Ambient, AmbientPayload};
+use reqwest::StatusCode;
+
+mod secrets;
+
+fn main() {
+    //const CHANNEL_ID: u32 = 12345;
+    //const WRITE_KEY: &str = "1234";
+    let dummy_data = vec![12.3, 45.6, 78.9];
+
+    let ambient = Ambient::new(secrets::ambient::CHANNEL_ID, String::from(secrets::ambient::WRITE_KEY));
+    let payload = AmbientPayload {
+        //created: Some(Utc::now()), Persing chrono::DataTime is not supported yes.
+        created: None,
+        d1: Some(dummy_data[0]),
+        d2: Some(dummy_data[1]),
+        d3: Some(dummy_data[2]),
+        d4: None,
+        d5: None,
+        d6: None,
+        d7: None,
+        d8: None,
+    };
+
+    let response = ambient.send(&payload, None);
+    match &response{
+        Ok(res) =>  {
+            match res.status() {
+                StatusCode::OK => println!("success!"),
+                StatusCode::PAYLOAD_TOO_LARGE => {
+                    println!("Request payload is too large!");
+                }
+                s => println!("Received response status: {:?}", s),
+            };
+        },
+        Err(error) => {
+            panic!("Http post failled.: {:?}", error);
+        }
+    }
+
+    assert_eq!(response.unwrap().status(), StatusCode::OK);
+}
